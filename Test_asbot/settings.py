84d1fe7b1c99c2ca138bd4fbbace7b7a2d4c5bfe
rostykpython <89851597+rostykpython@users.noreply.github.com>
2022-02-07 22:09:24 +0200

    some github changes

--- Test_asbot/settings.py
+++ Test_asbot/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_project',
         'USER': 'postgres',
-        'PASSWORD': '49046126w',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
